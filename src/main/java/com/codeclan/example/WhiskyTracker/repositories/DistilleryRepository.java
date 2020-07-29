86b7f3dc9f4915364c99ae7c227eff6b95fafcf8
KeithC113 <campbell_ka@yahoo.co.uk>
2020-07-29 14:50:05 +0100

    whiskies by year working

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository.java
@@ -4,4 +4,8 @@ import com.codeclan.example.WhiskyTracker.models.Distillery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DistilleryRepository extends JpaRepository<Distillery, Long> {
+
+
+
+
 }
